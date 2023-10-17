cf3d7a10727b2c8e10c79b7206b3cdd755202dec
arey-pranay <pranayparikh2004@gmail.com>
2023-10-17 17:16:59 +0530

    idkkk

--- client/src/App.js
+++ client/src/App.js
@@ -9,16 +9,16 @@ function App() {
   const [room, setRoom] = useState("");
 
   // Messages States
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const [messageReceived, setMessageReceived] = useState([]);
-  
+
   //Messenger State
-  const[myId, setMyId] = useState();
+  const [myId, setMyId] = useState();
   const [messengerId, setMessengerId] = useState("");
 
   //Message Time
-  const [messageTime, setMessageTime] = useState()
+  const [messageTime, setMessageTime] = useState();
 
   //Message Details
   const [messageDeet, setMessageDeet] = useState({});
@@ -36,33 +36,19 @@ function App() {
     setMessageDeet({
       message,
       messengerId,
-      messageTime
-    })
+      messageTime,
+    });
     socket.emit("send_message", { messageDeet, room });
     setMessages((existingMessages) => {
       const updatedMessages = [...existingMessages, message];
-      console.log(updatedMessages); // Add this line for debugging
+      // console.log(updatedMessages); // Add this line for debugging
       return updatedMessages;
     });
   };
-
-
   useEffect(() => {
-    // Move the socket.on("getId") outside of the joinRoom function
-    socket.on("getId", (id) => {
-      console.log(id);
-      setMyId(id);
-      console.log(myId);
-      alert(id);
-    });
-  }, []);
-
-  useEffect(() => {
-
     socket.on("receive_message", (messageDetails) => {
       setMessageReceived((existingMessages) => {
         const updatedMessages = [...existingMessages, messageDetails.message];
-        console.log(updatedMessages); // Add this line for debugging
         return updatedMessages;
       });
       setMessengerId(messageDetails.user);
@@ -80,50 +66,63 @@ function App() {
   };
 
   const handleEnterRoom = (e) => {
-    if(e.key === "Enter"){
-        joinRoom(e);
+    if (e.key === "Enter") {
+      joinRoom(e);
     }
-  }
+  };
 
+  const [elementHeight, setElementHeight] = useState(0);
+  useEffect(() => {
+    const windowHeight = window.innerHeight;
+    const headerHeight =
+      document.getElementById("HeadRoomElement").offsetHeight;
+    const footerHeight =
+      document.getElementById("FooterSendElement").offsetHeight;
+    const calculatedHeight = windowHeight - headerHeight - footerHeight;
+
+    setElementHeight(calculatedHeight);
+
+    console.log(elementHeight);
+  }, []);
   return (
-    <div className="flex flex-col justify-start p-4 w-full items-center">
-      <div>
+    <div className="flex flex-col overflow-x-hidden bg-blue-800 justify-start pt-4 w-full items-center">
+      <div id="HeadRoomElement">
         {" "}
         <input
+          className="p-2 outline-none"
           placeholder="Room Number..."
           onChange={(event) => {
             setRoom(event.target.value);
           }}
           onKeyDown={handleEnterRoom}
         />
-        <button className="bg-red-100 px-3 rounded-md py-1" onClick={joinRoom}>
+        <button
+          className="bg-red-100 px-3 mx-4 rounded-md py-2 hover:bg-red-500"
+          onClick={joinRoom}
+        >
           {" "}
           Join Room
         </button>
       </div>
-      <div className="flex flex-col justify-around w-screen mt-4 bg-cyan-100">
+      {console.log(elementHeight)}
+
+      <div
+        className={`flex flex-col text-gray-500 justify-start w-screen h-[${elementHeight}px] overflow-y-scroll mt-4 bg-cyan-100`}
+      >
         {messages.map((message) => {
           return (
             <>
-              <h2 className="ml-12" style={{ color: "red" }}>
-                {message}
-              </h2>
-            </>
-          );
-        })}
-        {messageReceived.map((message) => {
-          return (
-            <>
-              <h2>{message}</h2>
+              <h2 className="ml-12">{message}</h2>
             </>
           );
         })}
       </div>
-      {/* <h2>{messageReceived}</h2> */}
-      {/* <p>{messengerId}</p> */}
-      <div className="w-screen px-12 border border-t-red-800 py-4 flex justify-between">
+      <div
+        id="FooterSendElement"
+        className="w-screen px-12 border bg-white border-blue-100 py-4 flex justify-between"
+      >
         <input
-          className="w-full mr-4 border-none outline-none"
+          className="w-full mr-4 px-3 border-none outline-none"
           placeholder="Message..."
           onChange={(event) => {
             setMessage(event.target.value);
@@ -131,7 +130,7 @@ function App() {
           onKeyDown={handleEnter}
         />
         <button
-          className="bg-yellow-100 pl-8 w-32 rounded-md py-3 flex"
+          className="bg-yellow-100 hover:bg-yellow-400 pl-8 w-32 rounded-md py-3 flex"
           onClick={sendMessage}
         >
           {" "}
